bc8d039d9e9842cab4719f49e76e415c67964989
jayd21 <305898636@qq.com>
2020-05-12 19:01:12 +0800

    提交boc provider

--- cmd/virtual-kubelet/register.go
+++ cmd/virtual-kubelet/register.go
@@ -2,12 +2,25 @@ package main
 
 import (
 	"github.com/virtual-kubelet/virtual-kubelet/cmd/virtual-kubelet/internal/provider"
+	"github.com/virtual-kubelet/virtual-kubelet/cmd/virtual-kubelet/internal/provider/boc"
 	"github.com/virtual-kubelet/virtual-kubelet/cmd/virtual-kubelet/internal/provider/mock"
 )
 
 func registerMock(s *provider.Store) {
 	s.Register("mock", func(cfg provider.InitConfig) (provider.Provider, error) { //nolint:errcheck
 		return mock.NewMockProvider(
+			cfg.ConfigPath,
+			cfg.NodeName,
+			cfg.OperatingSystem,
+			cfg.InternalIP,
+			cfg.DaemonPort,
+		)
+	})
+}
+
+func registerBoc(s *provider.Store) {
+	s.Register("boc", func(cfg provider.InitConfig) (provider.Provider, error) { //nolint:errcheck
+		return boc.NewBocProvider(
 			cfg.ConfigPath,
 			cfg.NodeName,
 			cfg.OperatingSystem,
